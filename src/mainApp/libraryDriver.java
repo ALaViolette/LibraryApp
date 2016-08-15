9578726987e72eca28bddca99dcb8065920a2af9
Blair Brown <blair.j.brown@gmail.com>
2016-08-15 13:41:57 -0400

    added gitignore

--- src/mainApp/libraryDriver.java
+++ src/mainApp/libraryDriver.java
@@ -4,7 +4,7 @@ public class libraryDriver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+// hey there cuties. this is going to be the best project ever.  
 	}
 
 }
